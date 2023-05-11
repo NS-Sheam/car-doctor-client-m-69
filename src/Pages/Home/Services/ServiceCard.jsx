bf87eb3a1a466013b320f80bb707ce198266f8c8
NS-Sheam <123sheamfeni@gmail.com>
2023-05-11 13:54:15 +0600

    module 69 done

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,16 +1,19 @@
 
+import { FaArrowRight } from 'react-icons/fa';
 const ServiceCard = ({ service }) => {
     const { title, img, price } = service;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-                <img src={img} alt="Shoes" className="rounded-xl" />
+        <div className="card bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10 h-3/4">
+                <img src={img} alt="Shoes" className="rounded-xl h-full" />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                <p className="text-xl text-orange-500 font-bold">Price: ${price}</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div className="flex justify-between items-center">
+                    <p className="text-xl text-orange-500 font-bold">Price: ${price}</p>
+                    <button className="btn btn-circle btn-outline text-orange-500 border-orange-500 hover:bg-orange-500 hover:border-0">
+                    <FaArrowRight></FaArrowRight>
+                    </button>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -11,11 +11,11 @@ const Services = () => {
     return (
         <div className="mt-4">
             <div className="text-center lg:w-1/2 mx-auto">
-                <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
-                <h2 className="text-5xl">Our Service Area</h2>
+                <h3 className="lg:text-2xl font-bold text-orange-600">Our Services</h3>
+                <h2 className="text-2xl lg:text-5xl">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don not look even slightly believable. </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
                 {
                     services.map(service => <ServiceCard
                         key={service._id}
